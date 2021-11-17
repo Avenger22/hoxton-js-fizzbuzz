44d07cc45ccb6534e0fcebe916570ef70af1a516
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2021-11-17 17:05:52 +0100

    did the challenge, added also divisible by 7 and 9 from 1000 numbers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,25 @@
 let number = 1
 
-while (number <= 100) {
+while (number <= 1000) { // ONLY 105x1, 105x2 etc... IS FIZZBUZFISH AND ONLY 945 IS FIZZBUZZFISHBUS
+    if (number % 3 === 0 && number % 5 === 0 && number % 7 === 0 && number % 9 === 0) {
+        console.log("Number is FizzBuzzFishBus")
+    }
+    else if (number % 3 === 0 && number % 5 === 0 && number % 7 === 0) {
+        console.log("Number is FizzBuzzFish")
+    }
 
-    if (number % 3 === 0 && number % 5 === 0) {
+    else if (number % 3 === 0 && number % 5 === 0) {
         console.log("Number is FizzBuzz")
     }
+
     else if (number % 3 === 0) {
         console.log("Number is Fizz")
     }
+
     else if (number % 5 === 0) {
         console.log("Number is Buzz")
     }
+
     else {
         console.log(`Number is : ${number}`)
     }
